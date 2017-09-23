42383d2390102142bf44b0236916a7cf563a7e4d
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2017-09-23 09:51:57 +0200

    test for operator precedence

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -264,3 +264,23 @@ func TestParsingInfixExpressions(t *testing.T) {
 		}
 	}
 }
+
+func TestOperatorPrecendenceParsing(t *testing.T) {
+	tests := []struct {
+		input string
+		expected string
+	}{
+		{ "-a * b", "((-a)*b)" },
+	}
+
+	for _, tt := range tests {
+		l := lexer.New(tt.input)
+		p := New(l)
+		program := p.ParseProgram()
+
+		actual := program.String()
+		if actual != tt.expected {
+			t.Errorf("expected=%q got=%q", tt.expected, actual)
+		}
+	}
+}
